c7524e02d4f95ed7937fdbb0b4351c323087a6c1
gectokot <dmitry.kulak11@gmail.com>
2021-08-08 20:54:05 +0300

    Moved Dropdown styles to separate file

--- src/components/DropdownButton.tsx
+++ src/components/DropdownButton.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Dropdown, { Option } from "react-dropdown";
 
 import "react-dropdown/style.css";
+import "./DropdownButton.css"
 import { ItemInformation } from "../types";
 
 const DropdownButton = ({
