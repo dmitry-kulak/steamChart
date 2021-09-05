f23e4f84fb700e0edb1446f4727f4964b39c0bb7
gectokot <dmitry.kulak11@gmail.com>
2021-09-06 00:13:45 +0300

    heroku setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
-import express from 'express';
-import fs from 'fs';
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 const itemsData = JSON.parse(fs.readFileSync('./data/itemsData.json', 'utf-8'));
 const itemsList = JSON.parse(fs.readFileSync('./data/itemsList.json', 'utf-8'));
 
 const app = express();
+
+const staticFiles = express.static(path.join(__dirname, '../client/build'))
+
 app.use(express.json());
+app.use(staticFiles)
 
 app.get('/api/items', (req, res) => {
   return res.send(itemsList);
@@ -15,4 +20,6 @@ app.get('/api/chart/:id', (req, res) => {
   return res.send(itemsData.find((item) => item.id === Number(req.params.id)));
 });
 
-app.listen(process.env.PORT || 3001);
+app.use('/*', staticFiles)
+
+app.listen(process.env.PORT || 5000, () => console.log('server is running'));
